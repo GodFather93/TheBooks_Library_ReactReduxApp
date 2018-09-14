3af8d186ded393119dbb4d5c90feda6e1b48261a
GodFather93 <suryapalsingh.inda@gmail.com>
2018-09-14 21:40:36 +0530

    Added All Content

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "JavaScript: The Good parts", pages: 101 },
+    { title: "Harry Potter", pages: 39 },
+    { title: "The Dark Tower", pages: 10 },
+    { title: "Eloquent Ruby", pages: 3 }
+  ];
+}
